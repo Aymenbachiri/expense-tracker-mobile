75cf0fd0deae72c4b873f77f3a275b4731e61272
Aymenbachiri <aymen.bachiri99@gmail.com>
2025-07-01 10:13:48 +0100

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index(): React.JSX.Element {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500">
+        Edit app/index.tsx to edit this screen.nn
+      </Text>
+      <Text>gg</Text>
+    </View>
+  );
+}
